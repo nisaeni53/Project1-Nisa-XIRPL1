06b36601b0a394d8a3c91c1d9221b759378e5898
Nisa <nisaeni53@gmail.com>
2021-03-13 17:54:27 +0000

    Implementasi Kelas Daftar Menu

--- AplikasiRestoran/src/classes/DaftarMenu.java
+++ AplikasiRestoran/src/classes/DaftarMenu.java
@@ -11,16 +11,36 @@ import java.util.ArrayList;
  *
  * @author DELL LATITUDE E6430s
  */
+
+
+
 public class DaftarMenu {
 
     private ArrayList<Menu> daftarmenu;
+    
     public DaftarMenu(){
         daftarmenu =new ArrayList<>();
     }
     public void tambahMenu (Menu menu) {
         daftarmenu.add(menu);
     }
-    public void getMenuByKategori(String kategori){}
-    public void tampilDaftarMenu() {}
+    public void getMenuByKategori(String kategori){
+    System.out.println("======== " + kategori + "========");
+    
+    for (int i = 0; i < daftarmenu.size(); i++) {
+        Menu m = daftarmenu.get(i);
+        if (m.getKategori().equals(kategori)) {
+            System.out.println((i + 1) + ". " + m.getNama_menu() + "\t" + m.getHarga());
+        }
+    }
+    }
+    
+    public void tampilDaftarMenu() {
+        System.out.println("======== ALDEBARAMEN ========");
+        getMenuByKategori("Ramen");
+        getMenuByKategori("Kuah");
+        getMenuByKategori("Toping");
+        getMenuByKategori("Minuman");
+    }
     
 }
