43bb36fafe2f4b262ac9e226bccbc4c00ffa7cbb
Nisa <nisaeni53@gmail.com>
2021-03-16 10:41:31 +0000

    Modul D: Mencetak Struk

--- AplikasiRestoran/src/classes/Transaksi.java
+++ AplikasiRestoran/src/classes/Transaksi.java
@@ -20,6 +20,8 @@ public class Transaksi {
     private double uangBayar;
     private double pajak;
     private double totalBayar;
+    private double biayaService;
+    private double biayaPajak;
     
     public Transaksi(String no_transaksi, String nm_pemesan, String tanggal, String no_meja){
         this.noTransaksi = no_transaksi;
@@ -42,7 +44,71 @@ public class Transaksi {
         return pesanan;
     }
     
-    public double hitungTotalBayar() {return 0;}
-    public double hitungKembalian() {return 0;}
-    public void cetakStruk(){}
+    public double hitungTotalPesanan(){
+        
+        for (int i = 0; i < pesanan.size(); i++) {
+            Pesanan pan = pesanan.get(i);
+            double harga = pan.getMenu().getHarga();
+            totalBayar += (harga * pan.getJumlah());
+        }
+        return totalBayar;
+    }
+    
+    //tambahkan
+    public double hitungPajak(){
+        return totalBayar * pajak;
+    }
+    
+    //tambahkan
+    public double hitungBiayaService(){
+        return totalBayar * biayaService;
+    }
+    
+    //tambahkan
+    public double hitungTotalBayar(double pajak, double service) {
+        totalBayar = totalBayar + pajak + service;
+        return totalBayar;
+    }
+    
+    public double hitungKembalian(double uang_bayar) {
+        return uang_bayar - totalBayar; //bisa dibuat validator?
+    }
+    
+    public void cetakStruk(){
+        System.out.println("\n======== ALDEBARAMEN ========");
+        System.out.println("No Transaksi : "+noTransaksi);
+        System.out.println("Pemesan : "+namaPemesan);
+        System.out.println("Tanggal : "+tanggal);
+        
+        //cek jika nomor meja kosong, berarti take away
+        if(noMeja.equals("")){
+            noMeja = "Take Away";
+        }
+        
+        System.out.println("Meja : "+noMeja);
+        System.out.println("============================");
+        for (int i = 0; i < pesanan.size(); i++) {
+            Pesanan pan = pesanan.get(i);
+            Menu m = pan.getMenu();
+            String pesanan = pan.getJumlah() + " " + m.getNama_menu() + "\t" + (m.getHarga()*pan.getJumlah());
+            
+            //jika pesanan kuah, tambah spasi di awal 2
+            if (m.getKategori().equals("Kuah")) {
+                pesanan = " "+pesanan;
+            }
+            //tampilan pesanan
+            System.out.println(pesanan);
+            
+        }
+    }
+    
+    //tambahkan
+    public void setBiayaService(double service){
+        this.biayaService = service;
+    }
+    
+    //tambahkan
+    public void setPajak(double pajak){
+        this.biayaPajak = pajak;
+    }
 }
